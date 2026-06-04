104f9ce794e8a9bee2ba7eeee7b627ff1a436a39
karinamc5 <kmcapener@students.everettcc.edu>
2026-06-04 11:24:39 -0700

    menu

---
+++ scripts/mobile-nav.js
@@ -0,0 +1,25 @@
+// fetch('nav.html')
+
+//  .then(response => response.text())
+
+//  .then(data => {
+
+// document.getElementById('nav-placeholder').innerHTML = data;
+
+// Wait until nav.html is loaded, THEN attach toggle behavior
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navbarLinks = document.getElementsByClassName('hamish')[0];
+
+if (toggleButton && navbarLinks) {
+
+toggleButton.addEventListener('click', () => {
+
+navbarLinks.classList.toggle('active');
+
+ });
+
+ }
+
+// });
